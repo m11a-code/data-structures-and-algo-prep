4954598ea4ebdd9c12897651bbdc5a76e0315eb4
Matthew Ibarra <msibarr@gmail.com>
2022-06-27 16:04:48 -0400

    Basic SelectionSort implementation.

--- 01 Sorting/SelectionSort/Java/src/SelectionSort.java
+++ 01 Sorting/SelectionSort/Java/src/SelectionSort.java
@@ -5,18 +5,21 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] items = {1, 4, 3, 6, 6, 4, 3, 7, 9, 87, 234, 56, 3};
         System.out.println("Input array: " + Arrays.toString(items));
-        System.out.println("Output array:" + Arrays.toString(selectionsort(items)));
+        System.out.println("Output array:" + Arrays.toString(selectionSort(items)));
     }
 
-    private static int[] selectionsort(int[] items) {
+    private static int[] selectionSort(int[] items) {
+        int minIndex;
         for (int i = 0; i <= items.length - 1; i++) {
-            int minIndex = i;
+            minIndex = i;
             for (int j = i + 1; j <= items.length - 1; j++) {
                 if (items[j] < items[minIndex]) {
                     minIndex = j;
                 }
             }
-            swap(items, minIndex, i);
+            if (i != minIndex) {
+                swap(items, i, minIndex);
+            }
         }
         return items;
     }
