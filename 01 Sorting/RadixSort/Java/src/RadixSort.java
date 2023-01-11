49b319a5332c79a72967064ab2c0c862ed49965e
Matthew Ibarra <msibarr@gmail.com>
2023-01-11 11:54:11 -0500

    Radix sort implementation that is stable with a slightly hack-y tweak to support negative numbers without being too complicated. I think that deserves a 🎉. Added some print statements to show each step as the code moves through the significant digits. See CountingSort.java in repo for comments explaining counting sort impl. Only change was counting sort is manipulating the item value to get the appropriate significant digit and adding 9 to handle negative numbers. E.g., count array is size 19 to support digits that are from -9 to 9, e.g., [-9, -8, -7, ..., 7, 8, 9].

--- 01 Sorting/RadixSort/Java/src/RadixSort.java
+++ 01 Sorting/RadixSort/Java/src/RadixSort.java
@@ -3,46 +3,77 @@ import java.util.Arrays;
 public class RadixSort {
 
     public static void main(String[] args) {
-        int[] items = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 23, 4235, 3, 53, 45, 522, 3234, 222, 4345, 7, 89, 45,
-                765, 68, 54, 465, 23, 42, 457, 8, 786, 574, 563, 45, 45, 65, 78};
-        System.out.println("Input array: " + Arrays.toString(items));
-        System.out.println("Output array:" + Arrays.toString(radixsort(items)));
+        int[] items = {0, 1, 2, 3, 4, -1, -6, -6, 7, 8, 9, 10, -100, 23, -4235, 3, 53, 45, 522, 3234, 222, 4345, 7,
+                89, 45, 765, 68, 54, 465, 23, 42, 457, 8, 786, 574, 563, 45, 45, 65, 78};
+        System.out.println("Initial input array:");
+        System.out.println(Arrays.toString(items));
+        System.out.println();
+
+        int[] result = radixSort(items);
+
+        System.out.println("Final output array:");
+        System.out.println(Arrays.toString(result));
     }
 
     private static void countingSort(int[] items, int place) {
-        int[] count = new int[10];
+        int[] count = new int[19];
+        // [-9, -8, -7, ..., 7, 8, 9] -> size of 19.
+
+        System.out.println("Input array for " + place + " calculation:");
+        System.out.println(Arrays.toString(items));
 
         // Phase 1: Count
         // Calculate count of elements
         for (int item : items) {
-            ++count[(item / place) % 10];
+            int digit = ((item / place) % 10) + 9;
+            ++count[digit];
         }
+
+        System.out.println("Count array for " + place + " position:");
+        System.out.println(Arrays.toString(count));
 
         // Phase 2: Aggregate
         // Calculate cumulative frequency
-        for (int i = 1; i < 10; ++i) {
+        for (int i = 1; i < 19; ++i) {
             count[i] += count[i - 1];
         }
 
+        System.out.println("Aggregated count array for " + place + " position:");
+        System.out.println(Arrays.toString(count));
+
         // Phase 3: Write to target array
         // Place the elements in sorted order
+        // NOTE: see CountingSort.java for more detailed comments on CountingSort code and how it works.
+        // This code is only modified from original CountingSort code to isolate the single digit
+        // we're examining and +9 to support negative numbers.
         int[] target = new int[items.length];
         for (int i = items.length - 1; i >= 0; --i) {
-            int element = (items[i] / place) % 10;
+            int element = ((items[i] / place) % 10) + 9;
             target[count[element] - 1] = items[i];
             --count[element];
         }
 
         System.arraycopy(target, 0, items, 0, items.length);
+        System.out.println("Updated array after " + place + " calculation:");
+        System.out.println(Arrays.toString(items));
+        System.out.println();
     }
 
     // Function to implement radix sort
-    private static int[] radixsort(int[] items) {
+    private static int[] radixSort(int[] items) {
+
+        if (items == null) {
+            return null;
+        }
+        if (items.length <= 1) {
+            return items;
+        }
 
         // Get the max element in the array.
         int max = Arrays.stream(items).max().orElseThrow();
 
-        // Apply counting sort to sort elements based on place value.
+        // Apply a slightly modified counting sort to sort elements based on the significant digit that is currently
+        // being examined while still being able to handle negative numbers.
         for (int place = 1; max / place > 0; place *= 10) {
             countingSort(items, place);
         }
